600b9ec712babad002b01f3415dd41895b951b45
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-15 16:18:53 +0100

    Add Correction ToolBox (Command)

---
+++ Demo-ToolBox-Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using Demo_ToolBox;
+
+namespace Demo_ToolBox_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Command cmd = new Command("SELECT * FROM [student]");
+            Command cmd2 = new Command("SP_Student_GetById", new Dictionary<string, object?> {
+                { "student_id" , 13}
+            }, true);
+        }
+    }
+}
