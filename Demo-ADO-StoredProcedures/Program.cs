5f866f8e792e2a4e7c50664d727b1280e546329a
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-15 14:51:15 +0100

    Add Demo ADO.net Stored procedures

---
+++ Demo-ADO-StoredProcedures/Program.cs
@@ -0,0 +1,59 @@
+﻿using Demo_ADO_StoredProcedures.Services;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Demo_ADO_StoredProcedures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue!");
+            string userInput;
+            do
+            {
+                Console.WriteLine("1. Connecter");
+                Console.WriteLine("2. S'enregistrer");
+                Console.WriteLine("3. Quitter");
+                userInput = Console.ReadLine();
+                UserService service = new UserService();
+                switch (userInput) {
+                    case "1":
+                        Console.WriteLine("Quel est votre email ?");
+                        string email = Console.ReadLine();
+                        Console.WriteLine("Quel est votre mot de passe ?");
+                        string pwd = Console.ReadLine();
+                        if(service.Login(email, pwd))
+                        {
+                            Console.WriteLine($"Bonjour {email}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mauvaises informations.");
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("Quel est votre email ?");
+                        string email2 = Console.ReadLine();
+                        Console.WriteLine("Quel est votre mot de passe ?");
+                        string pwd2 = Console.ReadLine();
+                        try
+                        {
+                            service.Register(email2, pwd2);
+                            Console.WriteLine("Vous êtes inscrit!");
+                        }
+                        catch (Exception ex) {
+                            Console.WriteLine("Une erreur est survenue durant l'enregistrement.");
+                        }
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        Console.WriteLine("Veuillez choisir une option");
+                        break;
+                }
+            } while (userInput != "3");
+            Console.WriteLine("Bye bye!");
+        }
+    }
+}
---
+++ Demo-ADO-StoredProcedures/Services/UserService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_ADO_StoredProcedures.Services
+{
+    internal class UserService
+    {
+        const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WAD24-ADO-SQLServer;Integrated Security=True;Encrypt=False";
+        public void Register(string email, string password)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_User_Insert";
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlParameter p_email = new SqlParameter()
+                    {
+                        ParameterName = "email",
+                        Value = email
+                    };
+                    command.Parameters.Add(p_email);
+                    command.Parameters.AddWithValue("password", password);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public bool Login(string email, string password)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_User_CheckPassword";
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlParameter p_email = new SqlParameter()
+                    {
+                        ParameterName = "email",
+                        Value = email
+                    };
+                    command.Parameters.Add(p_email);
+                    SqlParameter p_password = new SqlParameter() { 
+                        ParameterName = "password",
+                        Value = password 
+                    };
+                    command.Parameters.Add(p_password);
+                    connection.Open(); 
+                    return (int)command.ExecuteScalar() == 1;
+                }
+            }
+        }
+    }
+}
