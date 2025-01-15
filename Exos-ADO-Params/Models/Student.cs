f2ad5c46ad2daaad819d99993e8aea6c9fff230c
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-15 12:27:39 +0100

    Add Correction ADO.net SqlParameters

---
+++ Exos-ADO-Params/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exos_ADO_Params.Models
+{
+    internal class Student
+    {
+        public int Student_Id { get; set; }
+        public string? First_Name { get; set; }
+        public string? Last_Name { get; set; }
+        public DateTime? Birth_Date { get; set; }
+        public string? Login { get; set; }
+        public int? Section_Id { get; set; }
+        public int? Year_Result { get; set; }
+        public string Course_Id { get; set; }
+
+        public Student(string? first_Name, string? last_Name, DateTime? birth_Date, int? section_Id)
+        {
+            First_Name = first_Name;
+            Last_Name = last_Name;
+            Birth_Date = birth_Date;
+            Section_Id = section_Id;
+            if(first_Name is not null && last_Name is not null)
+            {
+                Login = (first_Name.Substring(0,1) + last_Name.Substring(0,(last_Name.Length<7)?last_Name.Length : 7)).ToLower();
+            }
+            Course_Id = "0";
+        }
+    }
+}
---
+++ Exos-ADO-Params/Program.cs
@@ -0,0 +1,41 @@
+﻿using Exos_ADO_Params.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Exos_ADO_Params
+{
+    internal class Program
+    {
+        const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBSlide;Integrated Security=True;Encrypt=False";
+        static void Main(string[] args)
+        {
+            Student student = new Student("Nicole", "Lenoir", new DateTime(1975, 1, 1), 2425);
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand()) {
+                    command.CommandText = "INSERT INTO [student] ([first_name],[last_name],[birth_date],[login],[section_id],[year_result],[course_id]) OUTPUT [inserted].[student_id] VALUES (@first_name, @last_name, @birth_date, @login, @section_id, @year_result, @course_id)";
+
+                    SqlParameter p_first_name = new SqlParameter()
+                    {
+                        ParameterName = "first_name",
+                        Value = (object?)student.First_Name ?? DBNull.Value
+                    };
+                    command.Parameters.Add(p_first_name);
+
+                    command.Parameters.AddWithValue("last_name", (object?)student.Last_Name ?? DBNull.Value);
+                    command.Parameters.AddWithValue("birth_date", (object?)student.Birth_Date ?? DBNull.Value);
+                    command.Parameters.AddWithValue("login", (object?)student.Login ?? DBNull.Value);
+                    command.Parameters.AddWithValue("section_id", (object?)student.Section_Id ?? DBNull.Value);
+                    command.Parameters.AddWithValue("year_result", (object?)student.Year_Result ?? DBNull.Value);
+                    command.Parameters.AddWithValue("course_id", student.Course_Id);
+
+                    connection.Open();
+                    student.Student_Id = (int)command.ExecuteScalar();
+                    connection.Close();
+                }
+            }
+
+            Console.WriteLine($"L'étudiante {student.First_Name} {student.Last_Name} a bien été inscrie avec l'identifiant {student.Student_Id}!");
+        }
+    }
+}
